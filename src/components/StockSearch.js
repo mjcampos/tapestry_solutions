eb15167bab9b205ec29dc704e7862aa4dd1e245f
Michael Campos <camposmj89@gmail.com>
2019-06-27 23:00:29 -0700

    Updated the StockSearch component to include more buttons in the HTML and a stock state that can be updated via the input tags

--- src/components/StockSearch.js
+++ src/components/StockSearch.js
@@ -2,21 +2,53 @@ import React, {Component} from 'react';
 import {InputGroup, FormControl, Button} from 'react-bootstrap';
 
 class StockSearch extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			stock: ""
+		}
+	}
+
 	render() {
 		return (
-			<InputGroup>
-				<InputGroup.Prepend>
-					<InputGroup.Text>Search For Stock</InputGroup.Text>
-				</InputGroup.Prepend>
+			<div>
+				<InputGroup>
+					<InputGroup.Prepend>
+						<InputGroup.Text>Search For Stock</InputGroup.Text>
+					</InputGroup.Prepend>
 
-				<FormControl
-					placeholder="ex: GOOG"
-				/>
+					<FormControl
+						placeholder="ex: GOOG"
+						value={this.state.stock}
+						onChange={e => this.setState({stock: e.target.value})}
+					/>
 
-				<InputGroup.Append>
-					<Button>Button</Button>
-				</InputGroup.Append>
-			</InputGroup>
+				</InputGroup>
+
+				<InputGroup>
+					<InputGroup.Append>
+						<Button>Update Now</Button>
+					</InputGroup.Append>
+
+					<InputGroup.Append>
+						<Button variant="danger">Clear History</Button>
+					</InputGroup.Append>
+				</InputGroup>
+
+				<InputGroup>
+					<InputGroup.Append>
+						<Button variant="success">Start Automatic Updates</Button>
+					</InputGroup.Append>
+
+					<select>
+						<option value="2">2</option>
+						<option value="5">5</option>
+						<option value="10">10</option>
+						<option value="30">30</option>
+					</select>
+				</InputGroup>
+			</div>
 		);
 	}
 }
