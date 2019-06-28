3fdfc1b6b73bfb6a75c07d4574162ad4d9562863
Michael Campos <camposmj89@gmail.com>
2019-06-27 17:10:56 -0700

    Initial setup for Tapestry Solutions app, including the installation of Bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+
+class App extends Component {
+	render () {
+		return (
+			<Container>
+				<h1 className="text-center">Tapestry Solutions</h1>
+			</Container>
+		);
+	}
+}
+
+export default App;
